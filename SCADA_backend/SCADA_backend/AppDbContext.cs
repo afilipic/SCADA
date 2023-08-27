df8559bdb04e8fe3f0fbf14881d07642aadebcbd
Anita Pajic <anitaapajic@gmail.com>
2023-08-27 20:16:27 +0200

    Added connection to database

---
+++ SCADA_backend/SCADA_backend/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SCADA_backend;
+
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "server=localhost;user=root;password=Anitapajic123;database=scadadb";
+        var serverversion = new MySqlServerVersion(new Version(8, 0, 32));
+        optionsBuilder.UseMySql(connectionString, serverversion);
+    }
+}
--- SCADA_backend/SCADA_backend/Program.cs
+++ SCADA_backend/SCADA_backend/Program.cs
@@ -1,6 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
+using SCADA_backend;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>();
+
+var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/db", async (AppDbContext context) => 
+{
+    await context.Database.OpenConnectionAsync();
+    return "Database connection successful!";
+});
 
 app.Run();
+
