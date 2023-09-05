9a9f4cf58fa34ef8ed544156b55a9e72ac7b3c36
Tamara <dzambic.sv43.2020@uns.ac.rs>
2023-09-05 22:46:00 +0200

    added tag service

---
+++ SCADA_frontend/src/app/models/Tag.ts
@@ -0,0 +1,26 @@
+export interface DOTag{
+    Id : string,
+    Description : string,
+    Address : number,
+
+}
+
+
+export interface DITag{
+    Id : string,
+    Description : string,
+    Address : number,
+}
+
+
+export interface AOTag{
+    Id : string,
+    Description : string,
+    Address : number,
+}
+
+export interface AITag{
+    Id : string,
+    Description : string,
+    Address : number,
+}
--- SCADA_frontend/src/app/services/tag.service.ts
+++ SCADA_frontend/src/app/services/tag.service.ts
@@ -1,9 +1,117 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AITag, AOTag, DITag, DOTag } from '../models/Tag';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TagService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+  
+  private headers = new HttpHeaders({
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  });
+
+  httpRoot = 'http://localhost:5001/tag/'
+
+
+//DIGITAL OUTPUT
+
+getAllDO(): Observable<any> {
+  return this.http.get<any>(this.httpRoot + 'DO');
+}
+
+addDO(tag : DOTag): Observable<any> {
+  return this.http.post<any>(this.httpRoot + 'DO', tag);
+
+}
+
+editDO(tag : DOTag): Observable<any> {
+  return this.http.put<any>(this.httpRoot + 'DO/' + tag.Id, tag);
+
+}
+
+deleteDO(tagId : number): Observable<any> {
+  return this.http.delete<any>(this.httpRoot + 'DO/' + tagId);
+
+}
+
+
+//DIGITAL INPUT
+
+getAllDI(): Observable<any> {
+  return this.http.get<any>(this.httpRoot + 'DI');
+}
+
+addDI(tag : DITag): Observable<any> {
+  return this.http.post<any>(this.httpRoot + 'DI', tag);
+
+}
+
+editDI(tag : DITag): Observable<any> {
+  return this.http.put<any>(this.httpRoot + 'DI/' + tag.Id, tag);
+
+}
+
+switchDI(tagId : number): Observable<any> {
+  return this.http.put<any>(this.httpRoot + 'DI/switch/' + tagId, null);
+
+}
+
+deleteDI(tagId : number): Observable<any> {
+  return this.http.delete<any>(this.httpRoot + 'DI/' + tagId);
+
+}
+
+//ANALOG OUTPUT
+
+getAllAO(): Observable<any> {
+  return this.http.get<any>(this.httpRoot + 'AO');
+}
+
+addAO(tag : AOTag): Observable<any> {
+  return this.http.post<any>(this.httpRoot + 'AO', tag);
+
+}
+
+editAO(tag : AOTag): Observable<any> {
+  return this.http.put<any>(this.httpRoot + 'AO/' + tag.Id, tag);
+
+}
+
+deleteAO(tagId : number): Observable<any> {
+  return this.http.delete<any>(this.httpRoot + 'AO/' + tagId);
+
+}
+
+//ANALOG INPUT
+
+getAllAI(): Observable<any> {
+  return this.http.get<any>(this.httpRoot + 'AI');
+}
+
+addAI(tag : AITag): Observable<any> {
+  return this.http.post<any>(this.httpRoot + 'AI', tag);
+
+}
+
+editAI(tag : AITag): Observable<any> {
+  return this.http.put<any>(this.httpRoot + 'AI/' + tag.Id, tag);
+
+}
+
+switchAI(tagId : number): Observable<any> {
+  return this.http.put<any>(this.httpRoot + 'AI/switch/' + tagId, null);
+
+}
+
+deleteAI(tagId : number): Observable<any> {
+  return this.http.delete<any>(this.httpRoot + 'AI/' + tagId);
+
+}
+
+
 }
